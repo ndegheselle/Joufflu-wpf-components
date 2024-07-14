08bb7a30ae690729aed5ef0327d208f8d7b32e3e
Nicolas de gheselle <nicolas@degheselle.fr>
2024-07-14 17:26:30 +0200

    Refractor namespaces and components organization

---
+++ Joufflu.Shared/BaseNavigation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joufflu.Shared
+{
+    public interface INavigationLayout : IPage
+    {
+        public void Show(IPage page);
+    }
+
+    public interface IPage
+    {
+        public INavigationLayout? Layout { get; set; }
+    }
+}
---
+++ Joufflu.Shared/Resources/Fonts/Icon.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace Joufflu.Shared.Resources.Fonts
+{
+    public class Icon : TextBlock
+    {
+    }
+}
