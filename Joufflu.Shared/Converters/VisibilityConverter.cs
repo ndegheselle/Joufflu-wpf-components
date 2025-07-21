e7af7142b5a7adf85feb04421526f387a40293b1
NicolasDeGheselle <nicolas@degheselle.fr>
2025-07-21 13:08:31 +0200

    Add visibility converter

---
+++ Joufflu.Shared/Converters/VisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Joufflu.Shared.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BooleanConverter lConverterBool = new BooleanConverter();
+            bool lVisible = (bool)lConverterBool.Convert(value, targetType, parameter, culture);
+            return lVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        { throw new NotImplementedException(); }
+    }
+}
